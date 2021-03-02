72f35256a2d6ca377bcadb0e04f66e29d76eac44
pj100master <pj100.master@pj100.biz>
2021-03-02 09:05:17 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,12 @@
 class PostsController < ApplicationController
 
- def index
-  @posts = Post.all
- end
-
-  def new
+  def index
+    @posts = Post.all.order(id: "DESC")
   end
-
+ 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
-end
-
- # コントローラーにコメントを追加してみる
-# 会社のChromebookでコメントを追加してみる
+ 
+ end
